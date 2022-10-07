8575cfbace5b034e32c8a7764c12ca2986ff1932
Daniel Miller <daniel.miller@agile6.com>
2022-10-07 09:27:21 -0700

    experiment with event based hotjar surveys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faQuestion, faThumbsUp, faThumbsDown, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
-import { MipsDecisionTree } from './components'
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faQuestion,
+  faThumbsUp,
+  faThumbsDown,
+  faCheck,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import { MipsDecisionTree } from "./components";
+import { Button } from "@trussworks/react-uswds";
 
-import '@trussworks/react-uswds/lib/index.css'
-import '@trussworks/react-uswds/lib/uswds.css'
-import './App.css';
+import "@trussworks/react-uswds/lib/index.css";
+import "@trussworks/react-uswds/lib/uswds.css";
+import "./App.css";
 
-library.add(faQuestion, faThumbsUp, faThumbsDown, faCheck, faTimes)
+library.add(faQuestion, faThumbsUp, faThumbsDown, faCheck, faTimes);
+
+const triggerSurvey = (window) => {
+  window.hj("event", "example_event");
+};
 
 function App() {
   return (
@@ -16,6 +27,16 @@ function App() {
         <h1>MIPS Eligibility Decision Tree - Prototype UI </h1>
       </header>
       <main>
+        <div style={{ marginTop: "2rem" }}>
+          <Button
+            big
+            outline
+            className="qpp-dt-btn-action"
+            onClick={() => triggerSurvey(window)}
+          >
+            Test Event Driven Survey
+          </Button>
+        </div>
         <MipsDecisionTree />
       </main>
     </div>
